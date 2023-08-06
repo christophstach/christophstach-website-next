9c47a02eb32575e46f1334b9975852b6b0326031
Christoph Stach <christoph.stach@gmail.com>
2023-08-06 12:21:58 +0200

    Updated experience and education

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -42,13 +42,12 @@ export default function AboutMe() {
         like <a href="https://angular.io/">Angular</a>,{" "}
         <a href="https://reactjs.org/">React</a>,{" "}
         <a href="https://vuejs.org/">VueJS</a>,{" "}
-        <a href="https://svelte.dev/">Svelte</a> and{" "}
+        <a href="https://svelte.dev/">Svelte</a>,
+        <a href="https://www.solidjs.com/">SolidJS</a> and{" "}
         <a href="https://tailwindcss.com/">Tailwind CSS</a>. As well as meta
         frameworks like <a href="https://nextjs.org/">Next.js</a>,{" "}
         <a href="https://remix.run/">Remix</a>, and{" "}
-        <a href="https://kit.svelte.dev/">SvelteKit</a>. Recently,{" "}
-        <a href="https://www.solidjs.com/">SolidJS</a> caught my attention and I
-        also create this website with it.
+        <a href="https://kit.svelte.dev/">SvelteKit</a>.
       </p>
     </div>
   );
--- components/EducationTimeline.tsx
+++ components/EducationTimeline.tsx
@@ -7,51 +7,48 @@ export function getEducationData() {
   return [
     {
       title: "M.Sc. Applied Computer Science",
-      institution: "HTW Berlin - University of Applied Sciences",
-      description:
-        'I am studying Applied Computer Science (Angewandte Informatik) at <a target="_blank" href="https://www.htw-berlin.de/">HTW Berlin</a>. I am currently working on my master\'s thesis. During my time at this university, I have completed several projects in the areas of bioinformatics, deep learning, data science, and web development.',
+      institution: "HTW Berlin",
       bullets: [
-        "Using generative adversarial networks to create images of human faces",
-        'Member of the <a target="_blank" href="https://campus-stories.htw-berlin.de/en/jahr/2022/in-drei-stunden-das-virus-identifizieren/">Covid-SpiNGS</a> project, developing an on-demand next-generation sequencing pipeline for COVID-19 detection',
-        'Exchange year in Taipei, Taiwan at the <a target="_blank" href="https://www.ntust.edu.tw/">National Taiwan University of Science and Technology (NTUST)</a>',
+        "Currently working on Master's thesis",
+        "Focus on Web, AI, Bio Informatics, Deep Learning and Data Science",
+        "Generate human face images using generative adversarial networks",
+        "Contribute to Covid-SpiNGS project, developing an on-demand next-generation sequencing pipeline for COVID-19 detection",
+        "Participate in exchange program at National Taiwan University of Science and Technology (NTUST) in Taipei, Taiwan",
       ],
-      from: new Date(2019, 9, 1),
-      to: new Date(2023, 8, 30),
+      from: new Date(2019, 11, 1),
+      to: new Date(),
       current: true,
     },
     {
       title: "B.Sc. Applied Computer Science",
-      institution: "HTW Berlin - University of Applied Sciences",
-      description:
-        'I graduated from <a target="_blank" href="https://www.htw-berlin.de/">HTW Berlin</a> with a Bachelor in Applied Computer Science. During this time, I specialized in data science and deep learning.',
+      institution: "HTW Berlin",
       bullets: [
-        'Exchange semester in Cuernavaca, Mexico at <a target="_blank" href="https://uninter.edu.mx/">Universidad Internacional (UNINTER)</a>',
-        "Bachelor thesis on transferring painting styles to photos using deep learning techniques",
+        "Complete Bachelor thesis on transferring painting styles to photos using Deep Learning techniques, Grade: 1.6",
+        "Participate in exchange semester at Universidad Internacional (UNINTER) in Cuernavaca, Mexico",
       ],
       from: new Date(2016, 3, 1),
       to: new Date(2019, 8, 30),
     },
     {
-      title: "IT Management Assistant",
+      title: "IT Management Assistant (IHK)",
       institution: "KBS Nordhorn",
-      description:
-        'As part of my apprenticeship at <a target="_blank" href="https://www.kortmann-beton.de/">Kortmann Beton</a>, I attended this business-related school where the theoretical knowledge needed for the apprenticeship was taught.',
+      bullets: [
+        "Apprenticeship at Kortmann Beton alongside KBS Nordhorn theoretical studies, Grade: 2.0",
+      ],
       from: new Date(2009, 8, 1),
-      to: new Date(2011, 6, 31),
-      bullets: [],
+      to: new Date(2011, 6, 30),
     },
     {
       title: "Information Technology Assistant",
       institution: "Berufskolleg Rheine",
-      description:
-        'I began my IT career path at the technical college in Rheine. <a target="_blank" href="https://www.berufskolleg-rheine.de/">Berufskolleg Rheine</a> offered various subjects like Programming, Database, etc. I graduated with a dual degree as an Information Technology Assistant and the general qualification to study at Germany\'s Universities of Applied Sciences (Fachhochschule).',
+      bullets: [
+        "Dual degree in Information Technology, covering programming, database management, and more, including university eligibility, Grade: 2.9",
+      ],
       from: new Date(2004, 8, 1),
-      to: new Date(2008, 6, 31),
-      bullets: [],
+      to: new Date(2008, 6, 30),
     },
   ];
 }
-
 export default function EducationTimeline() {
   const data = getEducationData();
 
@@ -76,11 +73,6 @@ export default function EducationTimeline() {
                 : " Present"}
             </Timeline.Time>
             <Timeline.Content>
-              <p
-                className="prose prose-sm prose-indigo mb-5 mt-4 max-w-none dark:prose-invert"
-                dangerouslySetInnerHTML={{ __html: item.description }}
-              />
-
               {item.bullets.length > 0 && (
                 <ul className="ml-4 list-disc text-sm">
                   {item.bullets.map((bullet, index) => {

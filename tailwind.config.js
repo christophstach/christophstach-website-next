945948a7f254570a51713caf70b3dc6dfe987c9c
Christoph Stach <christoph.stach@gmail.com>
2025-12-25 15:07:19 +0100

    refactor: move Tailwind CSS configuration and plugin imports from `tailwind.config.js` to `globals.css`

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class", '[data-theme="dark"]'],
-  content: ["./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}"],
-  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
-};
